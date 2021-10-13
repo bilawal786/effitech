b15f0e6ddc5ce3e4e0ccf3abf4076d2c210da636
sonu <bilawal.jabbar.786@gmail.com>
2021-10-13 17:15:06 +0500

    add big update provider dashboard

---
+++ database/migrations/2021_10_13_111055_create_contents_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('image1');
+            $table->string('image2');
+            $table->string('image3');
+            $table->string('h1');
+            $table->string('h2');
+            $table->string('h3');
+            $table->string('h4');
+            $table->string('h5');
+
+            $table->string('st1');
+            $table->string('st2');
+            $table->string('st3');
+            $table->string('st4');
+            $table->string('st5');
+            $table->string('st6');
+
+            $table->string('sd1');
+            $table->string('sd2');
+            $table->string('sd3');
+            $table->string('sd4');
+            $table->string('sd5');
+            $table->string('sd6');
+
+            $table->string('timage');
+            $table->longText('review1');
+            $table->longText('review2');
+            $table->string('rg1');
+            $table->string('rg2');
+
+            $table->string('logo');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('fax');
+            $table->string('email');
+            $table->string('facebook');
+            $table->string('instagram');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
